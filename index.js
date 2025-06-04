eddb696336c131cf8e29cc0dc4de6d4a6491e04e
Piuskevin3 <mpavatarpius@gmail.com>
2025-06-04 03:33:55 +0300

    Update all fields

--- index.js
+++ index.js
@@ -20,14 +20,15 @@ app.post('/submit-manifest', async (req, res) => {
         const client = await auth.getClient();
         const sheets = google.sheets({ version: 'v4', auth: client });
 
-        console.log("Body keys:", Object.keys(req.body));
-
-
         // console.log("Request full details:\n", JSON.stringify(req.body, null, 2));
 
-        const d = req.body; // <== FIXED: removed .General
 
-        // console.log("Parsed object:\n", JSON.stringify(d, null, 2));
+        const d = req.body.Entry?.General || {};
+
+        // console.log("Parsed General object:\n", JSON.stringify(d, null, 2));
+
+        console.log("Parsed General object:", JSON.stringify(req.body.Entry?.General, null, 2));
+
 
         const values = [[
             d.Event || '',
@@ -56,7 +57,9 @@ app.post('/submit-manifest', async (req, res) => {
             d.VehicleDetails?.VerifierName || ''
         ]];
 
-        // console.log("Parsed values:\n", values);
+        // console.log(values);
+
+
 
         await sheets.spreadsheets.values.append({
             spreadsheetId: SHEET_ID,
@@ -71,7 +74,6 @@ app.post('/submit-manifest', async (req, res) => {
         res.status(500).json({ success: false, message: 'Failed to save data' });
     }
 });
-
 
 
 const PORT = process.env.PORT || 4000;
