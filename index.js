c6fd97605a69a71a6aeb1f1697f830cfcb381172
Piuskevin3 <mpavatarpius@gmail.com>
2025-06-04 03:15:46 +0300

    Modify all fields

--- index.js
+++ index.js
@@ -20,21 +20,13 @@ app.post('/submit-manifest', async (req, res) => {
         const client = await auth.getClient();
         const sheets = google.sheets({ version: 'v4', auth: client });
 
-        // const d = req.body.General || {};
-        console.log("Request full details : "+req.body);
-        
-        const d = {
-            ...req.body.General,
-            Coordinator: req.body.General?.Coordinator || {},
-            DriversDetails: req.body.General?.DriversDetails || {},
-            VehicleDetails: req.body.General?.VehicleDetails || {},
-            SoulsDetails: {
-                ...req.body.General?.SoulsDetails,
-                Residents: req.body.General?.SoulsDetails?.Residents || {},
-                Institutions: req.body.General?.SoulsDetails?.Institutions || {},
-                Schools: req.body.General?.SoulsDetails?.Schools || {},
-            },
-        };
+        console.log("Request full details:\n", JSON.stringify(req.body, null, 2));
+
+
+        const d = req.body.General || {};
+
+        console.log("Parsed General object:\n", JSON.stringify(d, null, 2));
+
 
 
         const values = [[
