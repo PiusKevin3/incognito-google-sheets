f9c552677c2928794cef4c2f8fb0983ea02109d3
Odeke Trevor Angulo <41131535+Odeke12@users.noreply.github.com>
2025-07-24 00:21:50 +0300

    Add more logs

--- routes/financeRoutes.js
+++ routes/financeRoutes.js
@@ -74,6 +74,8 @@ router.post('/submit-finance', validateApiKey, async (req, res) => {
         ]];
 
         const newFinanceContribution = flatSection["FinanceContribution"] + flatSection["Amount"];
+        console.log(flatSection["FinanceContribution"], flatSection["Amount"]);
+
         console.log('New Finance Contribution:', newFinanceContribution);
 
         try {
