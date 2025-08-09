0e90f82c5e70630e4094160f26584667b80d246a
Odeke Trevor Angulo <41131535+Odeke12@users.noreply.github.com>
2025-08-09 05:43:33 +0300

    Add rejectUnauthorized: false

--- config/db.js
+++ config/db.js
@@ -33,7 +33,9 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
-  ssl: sslConfig
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
